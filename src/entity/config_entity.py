86a175eb9d891f6b293495717521c70d76e6c1f3
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-30 18:30:21 +0000

    adding src/entity for data validaiton

--- src/entity/config_entity.py
+++ src/entity/config_entity.py
@@ -7,3 +7,13 @@ class DataIngestionConfig:
     source_URL: str
     local_data_file: Path
     unzip_dir: Path
+
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE: str
+    unzip_data_dir:Path
+    all_schema:dict
