aaee47b1244658bd2be0ef82f00cf247f3d0d85e
rahulsamant37 <161972011+rahulsamant37@users.noreply.github.com>
2024-12-30 18:23:27 +0000

    Initializing main.py for data ingestion

---
+++ main.py
@@ -0,0 +1,12 @@
+from src import logger
+from src.pipeline.data_ingestion_pipeline import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.inititate_data_ingestion()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
